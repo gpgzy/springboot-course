43001b37e71d56df3a8dd1ecc20c170d7f890275
BO <bwang0629@gmail.com>
2020-04-20 13:25:05 +0800

    后端整合；CORSFilter

--- springboot-examples/README.md
+++ springboot-examples/README.md
@@ -1,1 +1,3 @@
 # Springboot Integration
+### 2020.04.19
+后端整合；与vue前端整合提供数据；后端CORSFilter(只有代码演示，最终跨域由前端解决)；  
--- springboot-examples/src/main/java/com/example/springbootexamples/WebMvcConfig.java
+++ springboot-examples/src/main/java/com/example/springbootexamples/WebMvcConfig.java
@@ -3,7 +3,12 @@ package com.example.springbootexamples;
 import com.example.springbootexamples.interceptor.LoginInterceptor;
 import com.example.springbootexamples.interceptor.TeacherInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,11 +23,22 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/api/**")
-                .excludePathPatterns("/api/login");
+                .excludePathPatterns("/api/login", "/api/test");
 
         registry.addInterceptor(teacherInterceptor)
                 .addPathPatterns("/api/teacher/**");
-
     }
 
+    /*@Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource configSource
+                = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(configSource);
+    }*/
 }
